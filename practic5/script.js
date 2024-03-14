c54687791871978d1d51d4ccfcf882fa8e5410ba
anya-hls <159822005+anya-hls@users.noreply.github.com>
2024-03-14 19:40:47 +0200

    Update script.js

---
+++ practic5/script.js
@@ -0,0 +1,10 @@
+var car1 = new Object();
+car1.color = "black";
+car1.maxSpeed = 18;
+car1.driver = {
+    name: "Halas Anna-Sofia",
+    category: "C",
+    personalLimitations: "No driving at night"
+};
+car1.tuning = true;
+car1.numberOfAccidents = 0;
