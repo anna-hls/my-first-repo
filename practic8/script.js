33f301dd435a48083e79a04568c374679a973e15
anya-hls <159822005+anya-hls@users.noreply.github.com>
2024-04-08 15:21:19 +0300

    Update script.js

--- practic8/script.js
+++ practic8/script.js
@@ -1,22 +1,26 @@
-import { speak as speakHello } from './library/SpeakHello.js';
-import { speak as speakGoodbye, speakAdd as speakAddGoodbye } from './library/SpeakGoodBye.js';
+import { speak as speakHello } from './SpeakHello.js';
+import { speak as speakGoodBye } from './SpeakGoodBye.js';
 
 var names = ["Bill", "John", "Jen", "Jason", "Paul", "Frank", "Steven", "Larry", "Paula", "Laura", "Jim"];
 
 for (var i = 0; i < names.length; i++) {
-    if (names[i][0].toLowerCase() === 'j') {
-        speakGoodbye(names[i]);
+    var firstLetter = names[i].charAt(0).toLowerCase();
+    if (firstLetter === 'j') {
+        speakGoodBye(names[i]);
     } else {
         speakHello(names[i]);
     }
 }
 
-var additionalSelection = function(name) {
-    if (name.charAt(name.length - 1).toLowerCase() === 'a') {
-        console.log(name + " ends with 'a'");
+function selectNamesByLength(names) {
+    var selectedNames = [];
+    for (var i = 0; i < names.length; i++) {
+        if (names[i].length % 2 === 0) {
+            selectedNames.push(names[i]);
+        }
     }
-};
+    return selectedNames;
+}
 
-names.forEach(function(name) {
-    additionalSelection(name);
-});
+var selectedNames = selectNamesByLength(names);
+console.log("Selected names with even length:", selectedNames);
