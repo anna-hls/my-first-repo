ee18f05a97c94c3fe1df5e3176b2f5958d773c79
anya-hls <159822005+anya-hls@users.noreply.github.com>
2024-04-12 18:08:14 +0300

    Update sorting.js

--- practic10/library/sorting.js
+++ practic10/library/sorting.js
@@ -1,1 +1,65 @@
+function exchange(arr, i, j) {
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
 
+function bubbleSort(arr, order) {
+    let comparisons = 0;
+    let exchanges = 0;
+    const n = arr.length;
+
+    for (let i = 0; i < n - 1; i++) {
+        for (let j = 0; j < n - i - 1; j++) {
+            comparisons++;
+            if ((order === 'asc' && arr[j] > arr[j + 1]) || (order === 'desc' && arr[j] < arr[j + 1])) {
+                exchange(arr, j, j + 1);
+                exchanges++;
+            }
+        }
+    }
+    console.log(`Bubble sort: comparisons - ${comparisons}, exchanges - ${exchanges}`);
+}
+
+function selectionSort(arr, order) {
+    let comparisons = 0;
+    let exchanges = 0;
+    const n = arr.length;
+
+    for (let i = 0; i < n - 1; i++) {
+        let minIndex = i;
+        for (let j = i + 1; j < n; j++) {
+            comparisons++;
+            if ((order === 'asc' && arr[j] < arr[minIndex]) || (order === 'desc' && arr[j] > arr[minIndex])) {
+                minIndex = j;
+            }
+        }
+        if (minIndex !== i) {
+            exchange(arr, i, minIndex);
+            exchanges++;
+        }
+    }
+    console.log(`Selection sort: comparisons - ${comparisons}, exchanges - ${exchanges}`);
+}
+
+function insertionSort(arr, order) {
+    let comparisons = 0;
+    let exchanges = 0;
+    const n = arr.length;
+
+    for (let i = 1; i < n; i++) {
+        let current = arr[i];
+        let j = i - 1;
+        comparisons++;
+        while (j >= 0 && ((order === 'asc' && arr[j] > current) || (order === 'desc' && arr[j] < current))) {
+            arr[j + 1] = arr[j];
+            j--;
+            comparisons++;
+            exchanges++;
+        }
+        arr[j + 1] = current;
+    }
+    console.log(`Insertion sort: comparisons - ${comparisons}, exchanges - ${exchanges}`);
+}
+
+export { bubbleSort, selectionSort, insertionSort };
